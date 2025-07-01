503f66d0e11dec99dbb4b7c6836e84f2f99021d3
Patel Urvil <uvpatel7271@gmail.com>
2025-07-01 20:12:07 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,10 +3,13 @@ from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
 app = Flask(__name__)
+
+# Database configuration
 app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///todo.db"
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# Todo model
 class Todo(db.Model):
     sno = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(200), nullable=False)
@@ -16,48 +19,48 @@ class Todo(db.Model):
     def __repr__(self) -> str:
         return f"{self.sno} - {self.title}"
 
-@app.route('/', methods=['POST','GET'])
-def hello_world():
+# Home route: Add and display tasks
+@app.route('/', methods=['POST', 'GET'])
+def index():
     if request.method == 'POST': 
         title = request.form['title']
         desc = request.form['desc']
-        todo = Todo(title=title,desc = desc)
+        todo = Todo(title=title, desc=desc)
         db.session.add(todo)
         db.session.commit()
-   
+        return redirect("/")
+    
     allTodo = Todo.query.all()
     return render_template('index.html', allTodo=allTodo)
 
-
-
-
+# Show route (debug)
 @app.route('/show')
 def products():
     allTodo = Todo.query.all()
     print(allTodo)
     return "Products World!"
 
+# Update route
 @app.route('/update/<int:sno>', methods=['GET', 'POST'])
 def update(sno):
+    todo = Todo.query.filter_by(sno=sno).first()
     if request.method == 'POST':
-        title = request.form['title']
-        desc = request.form['desc']
-        todo = Todo.query.filter_by(sno=sno).first()
-        todo.title = title
-        todo.desc = desc
-        db.session.add(todo)
+        todo.title = request.form['title']
+        todo.desc = request.form['desc']
         db.session.commit()
         return redirect("/")
-    
-    todo = Todo.query.filter_by(sno=sno).first()
     return render_template('update.html', todo=todo)
 
+# Delete route
 @app.route('/delete/<int:sno>')
 def delete(sno):
-    todo = Todo.query.filter_by(sno= sno).first()
+    todo = Todo.query.filter_by(sno=sno).first()
     db.session.delete(todo)
     db.session.commit()
     return redirect("/")
 
 if __name__ == "__main__":
+    # Create tables before starting the app
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
